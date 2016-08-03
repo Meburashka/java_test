e08cc0a1003407db8c26ff1c5d2cc0af5db2d179
Meburashka <katuha_21@mail.ru>
2016-08-03 17:58:16 +0300

    Задание №3: Подключить тестовый фреймворк и сделать первые тесты
    Подключен тестовый фреймворк. Сделан тест как в лекции.
    Написано два теста для Point:
    1. Проверка результата работы конструтора в классе Point и метода
    distance.
    2. Проверка результата работы метода distance в классе Point.

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/point/PointDesignerTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox.point;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.l1.Point;
+
+import static ru.stqa.pft.sandbox.l1.MainL1.distance;
+
+/**
+ * Created by SorEA on 03.08.2016.
+ */
+public class PointDesignerTests {
+
+  @Test
+  public void testDesigner() {
+    Point p1 = new Point(2, 5);
+    Point p2 = new Point(4, 7);
+
+    Assert.assertEquals(distance(p1, p2), 4.242640687119285);
+  }
+}
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/point/PointMethodTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox.point;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.l1.Point;
+
+import static ru.stqa.pft.sandbox.l1.MainL1.distance;
+
+/**
+ * Created by SorEA on 03.08.2016.
+ */
+public class PointMethodTests {
+
+  @Test
+  public void testDesigner() {
+    Point p = new Point(3, 4, 5, 7.2);
+
+    Assert.assertEquals(p.distance(), 2.4166091947189146);
+  }
+}
